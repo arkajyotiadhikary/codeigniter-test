dac7a7f17adc061ab2126eed702c6aebb84ca44c
Arka Jyoti Adhikary <arkajyotiadhikary15@gmail.com>
2022-08-29 14:32:46 +0530

    expand button symbol changed after expanding

--- index.js
+++ index.js
@@ -3,8 +3,13 @@
 const handleClick = (e) => {
     const expandDiv = e.parentNode.parentNode.childNodes[3];
     if (expandDiv.style.height === "0px") {
+        // console.log(e.childNodes[1].innerHTML);
+        e.childNodes[1].innerHTML = "remove";
         expandDiv.style.height = "168px";
+        expandDiv.childNodes[1].style.display = "block";
     } else {
         expandDiv.style.height = "0px";
+        e.childNodes[1].innerHTML = "add";
+        expandDiv.childNodes[1].style.display = "none";
     }
 };
